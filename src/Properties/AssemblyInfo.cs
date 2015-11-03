fd16b25656f63d8aff6946977f0e1d6a231d7b20
Team City <info@exira.com>
2015-11-03 12:26:35 +0000

    Bump version to 0.1.1

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("ges-runner")]
 [assembly: AssemblyProductAttribute("Exira.EventStore.Runner")]
 [assembly: AssemblyDescriptionAttribute("Exira.EventStore.Runner is a wrapper that uses Topshelf to run EventStore as a Windows Service")]
-[assembly: AssemblyVersionAttribute("0.1.0")]
-[assembly: AssemblyFileVersionAttribute("0.1.0")]
-[assembly: AssemblyMetadataAttribute("githash","dbdde047cf8aef5f672843ffa02286a9c38f3bc1")]
+[assembly: AssemblyVersionAttribute("0.1.1")]
+[assembly: AssemblyFileVersionAttribute("0.1.1")]
+[assembly: AssemblyMetadataAttribute("githash","d38ad61ff673ca34d69cb9e63b23de502372e70c")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.1.0";
+        internal const string Version = "0.1.1";
     }
 }
