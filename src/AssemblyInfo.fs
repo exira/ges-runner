2f8d02e320a52cab40b001169b0d44d6b4bf4f76
David Cumps <david@cumps.be>
2015-11-09 16:02:39 +0100

    Fix documentation generation

---
+++ src/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("ges-runner")>]
+[<assembly: AssemblyProductAttribute("Exira.EventStore.Runner")>]
+[<assembly: AssemblyDescriptionAttribute("Exira.EventStore.Runner is a wrapper that uses Topshelf to run EventStore as a Windows Service")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly: AssemblyMetadataAttribute("githash","e92e1abd29a7dc9f9a289889a8620655af1dde80")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.1.0"
