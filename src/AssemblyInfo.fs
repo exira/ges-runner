bfb5ae5bffd4cb7436f6d713a190df7a6caacb7b
Team City <info@exira.com>
2015-12-30 12:58:51 +0000

    Bump version to 1.3.26

--- src/AssemblyInfo.fs
+++ src/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("ges-runner")>]
 [<assembly: AssemblyProductAttribute("Exira.EventStore.Runner")>]
 [<assembly: AssemblyDescriptionAttribute("Exira.EventStore.Runner is a wrapper that uses Topshelf to run EventStore as a Windows Service")>]
-[<assembly: AssemblyVersionAttribute("1.3.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.3.0")>]
-[<assembly: AssemblyMetadataAttribute("githash","39769a710fd379678d7913356c1b091de0087b1e")>]
+[<assembly: AssemblyVersionAttribute("1.3.26")>]
+[<assembly: AssemblyFileVersionAttribute("1.3.26")>]
+[<assembly: AssemblyMetadataAttribute("githash","90e1eaa4f3902297bac9ad2ef3bd6f26450617e6")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.3.0"
+    let [<Literal>] Version = "1.3.26"
